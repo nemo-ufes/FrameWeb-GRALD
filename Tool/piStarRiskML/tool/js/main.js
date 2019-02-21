de3d5e29217a115ebe0ba1fc8f1cca2fb8e50c48
marciolf29 <lf.marcio@gmail.com>
2019-02-20 23:40:11 -0300

    Grald
    Grald

---
+++ Tool/piStarRiskML/tool/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    istar.setupModel();
+    istar.setupDiagram();
+    istar.setupMetamodel(istarcoreMetamodel);
+    examples.pistarIntro();
+    ui.setupUi();
+});
